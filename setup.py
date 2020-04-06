fe66f7b3398098f4bfd0b6923d5531581a559ab4
en1ma <amine_boudraa@outlook.fr>
2020-04-06 18:54:12 +0200

    enhancing setup.py

--- setup.py
+++ setup.py
@@ -1,41 +1,7 @@
-import sys
-
-def progress_module():
-    try:
-        import progress
-    except ModuleNotFoundError as error:
-        try:
-            choice = input('[*] Missing progress module, would you like to install it ? [yY/nN] ')
-        except KeyboardInterrupt:
-            print('[!] User keyboard interrupt !')
-            raise SystemExit
-        if choice.strip().lower()[0] == 'y':
-            print('[*] Trying to install progress...')
-            sys.stdout.flush()
-            try:
-                from pip._internal import main as pip
-                pip(['install', '--user', 'progress'])
-                print('[+] Installing progress...')
-                from progress.bar import Bar
-                print('[!] Successfully imported progress.bar')
-
-            except Exception:
-                print('[!] Failed to install progress.bar')
-                raise SystemExit
-        elif choice.strip().lower()[0] == 'n':
-            print('[+] Canceled : progress.bar module will not be installed')
-            raise SystemExit
-        else:
-            print('[!] Invalid user input')
-            raise SystemExit
-
-
-def install_modules():
-    progress_module()
-
+from pip._internal import main as pip
 
 def main():
-    install_modules()
+    pip(['install', '-r', 'requirements.txt'])
 
 if __name__ == "__main__":
     main()
