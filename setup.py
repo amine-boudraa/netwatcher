a24a2ac329f2393f2a7ad33da91036875953b5ef
en1ma <amine_boudraa@outlook.fr>
2020-04-10 15:22:55 +0200

    finalisation de la création du fichier de conf telegram

--- setup.py
+++ setup.py
@@ -1,25 +1,34 @@
 from pip._internal import main as pip
 import argparse
+import json
 
 def setup():
 
 
       parser = argparse.ArgumentParser(description="Configuration")
-      parser.add_argument('-u', '--telegramUserToken',type=int, help="Telegram user token")
-      parser.add_argument('-gid', '--telegramGroupID',type=int, help="Telegram group ID")
+      parser.add_argument('-tbn', '--telegramBotToken', help="Telegram bot token")
+      parser.add_argument('-tcid', '--telegramChatID' , help="Telegram chat ID")
       parser.add_argument('-r', '--requirements',action='store_true',help="Requirements")
 
       args = parser.parse_args()
 
-      if args.telegramUserToken and args.telegramGroupID:
-          generateTelegramConfiguration(args.telegramUserToken,args.telegramGroupID)
+      if args.telegramBotToken and args.telegramChatID:
+          generateTelegramConfiguration(args.telegramBotToken,args.telegramChatID)
 
       if args.requirements:
-             pip(['install', '-r', 'requirements.txt'])
+          pip(['install', '-r', 'requirements.txt'])
 
 
-def generateTelegramConfiguration(telegramUserToken,telegramGroupID):
-    print("TODO : Generer fichier de configuration avec les bons fields")
+def generateTelegramConfiguration(telegramBotToken,telegramChatID):
+      data = {}
+      data['telegram_configuration'] = []
+      data['telegram_configuration'].append({
+           'telegramBotToken': telegramBotToken,
+           'telegramChatID': telegramChatID,
+        })
+
+      with open('configuration.json', 'w') as outfile:
+         json.dump(data, outfile)
 
 def main():
     setup()
