5e2f0d676301f12a3fa45e1b603905e19bab1bd6
en1ma <amine_boudraa@outlook.fr>
2020-04-10 13:32:55 +0200

    modification structure setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,28 @@
 from pip._internal import main as pip
+import argparse
+
+def setup():
+
+
+      parser = argparse.ArgumentParser(description="Configuration")
+      parser.add_argument('-u', '--telegramUserToken',type=int, help="Telegram user token")
+      parser.add_argument('-gid', '--telegramGroupID',type=int, help="Telegram group ID")
+      parser.add_argument('-r', '--requirements',action='store_true',help="Requirements")
+
+      args = parser.parse_args()
+
+      if args.telegramUserToken and args.telegramGroupID:
+          generateTelegramConfiguration(args.telegramUserToken,args.telegramGroupID)
+
+      if args.requirements:
+             pip(['install', '-r', 'requirements.txt'])
+
+
+def generateTelegramConfiguration(telegramUserToken,telegramGroupID):
+    print("TODO : Generer fichier de configuration avec les bons fields")
 
 def main():
-    pip(['install', '-r', 'requirements.txt'])
+    setup()
 
 if __name__ == "__main__":
     main()
